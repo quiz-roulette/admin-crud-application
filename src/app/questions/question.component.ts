78324dfbb3db8a53dbe69ec146fcb33fb3342ade
Smit <smitshah95@hotmail.com>
2017-11-13 12:19:00 +0800

    added delete functionality

--- src/app/questions/question.component.ts
+++ src/app/questions/question.component.ts
@@ -40,7 +40,9 @@ export class QuestionComponent implements OnInit {
         else
         alert("Correct option not found the choices. Please ensure to use correct choice from the choices you wrote.");
     }
-    deleteQuestion(text,category,choice,correctchoice){
-        console.log("test");
+    deleteQuestion(questionId){
+        this.httpService.deleteQuestion(questionId).then((data) => {
+            alert("Delete Succesfully");
+        })
     }
 }
