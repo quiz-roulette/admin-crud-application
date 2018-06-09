3a7e38de5ce7d99c1d1d358fab2e08c1083816d5
Smit <smitshah95@hotmail.com>
2018-06-10 01:49:51 +0800

    Added checks for empty spaces

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, RequestOptions,Headers } from '@angular/http';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -13,54 +13,54 @@ import { Category } from '../model/category';
 @Injectable()
 export class HTTPService {
     private AzureUrl = "https://axperienceapp.azurewebsites.net";
-    options: any;    
-    
+    options: any;
+
 
 
 
 
     Login(quizUserId, password): any {
-        return this.http.get(`${this.AzureUrl}/api/authenticate?userId=${quizUserId}&userPassword=${password}&isAdmin=${true}`,this.options).toPromise().then(this.extractData);
+        return this.http.get(`${this.AzureUrl}/api/authenticate?userId=${quizUserId}&userPassword=${password}&isAdmin=${true}`, this.options).toPromise().then(this.extractData);
     }
 
     getAllQuizUsers(): any {
-        return this.http.get(`${this.AzureUrl}/api/quizuser`,this.options).toPromise().then(this.extractData);
+        return this.http.get(`${this.AzureUrl}/api/quizuser`, this.options).toPromise().then(this.extractData);
     }
 
 
-    constructor(private http: Http) { 
+    constructor(private http: Http) {
         this.options = new RequestOptions({ headers: new Headers({ 'ZUMO-API-VERSION': '2.0.0' }) });
     }
 
     /**
      * returns all the questions from the database
      */
-    getAllQuestions(){
-        return this.http.get(`${this.AzureUrl}/api/question`,this.options).toPromise().then(this.extractData);
+    getAllQuestions() {
+        return this.http.get(`${this.AzureUrl}/api/question`, this.options).toPromise().then(this.extractData);
     }
 
     /**
      * returns all the choices from the database
      */
-    getAllChoices(){
-        return this.http.get(`${this.AzureUrl}/api/choice`,this.options).toPromise().then(this.extractData);
+    getAllChoices() {
+        return this.http.get(`${this.AzureUrl}/api/choice`, this.options).toPromise().then(this.extractData);
     }
 
     /**
      * returns all the correctChoices from the database
      */
-    getAllCorrectChoices(){
-        return this.http.get(`${this.AzureUrl}/api/correctChoice`,this.options).toPromise().then(this.extractData);
+    getAllCorrectChoices() {
+        return this.http.get(`${this.AzureUrl}/api/correctChoice`, this.options).toPromise().then(this.extractData);
     }
 
-    getAllCategories(){
-        return this.http.get(`${this.AzureUrl}/api/category`,this.options).toPromise().then(this.extractData);
+    getAllCategories() {
+        return this.http.get(`${this.AzureUrl}/api/category`, this.options).toPromise().then(this.extractData);
     }
-    
+
     /**
      * Returns questionWrapper that consisits of Question,Choices and CorrectChoice.
      */
-    getAllQuestionWrapper(): Promise<QuestionWrapper[]>  {
+    getAllQuestionWrapper(): Promise<QuestionWrapper[]> {
         var current = this;
         var questionWrappers = new Array<QuestionWrapper>();
         var questions: Question[];
@@ -71,22 +71,22 @@ export class HTTPService {
             let promises = new Array<Promise<any>>();
 
             let promise = current.getAllQuestions()
-            .then(data => questions = data)
-            .catch(this.handleError);
+                .then(data => questions = data)
+                .catch(this.handleError);
 
             promises.push(promise);
 
             promise = current.getAllChoices()
-            .then(data => {
-                choices = data;
-            })
-            .catch(this.handleError);
+                .then(data => {
+                    choices = data;
+                })
+                .catch(this.handleError);
 
             promises.push(promise);
 
             promise = current.getAllCorrectChoices()
-            .then(data => correctChoices = data)
-            .catch(this.handleError);
+                .then(data => correctChoices = data)
+                .catch(this.handleError);
 
             promises.push(promise);
 
@@ -115,37 +115,41 @@ export class HTTPService {
      * layer 2: It loops for the choices(because there has to be more than 1 choice) and add one by one to the database
      * layer 3: It get the choiceId of the correctChoice and add that to the database.
      */
-    addQuestionWrapper(questionWrapper: QuestionWrapper): Promise<QuestionWrapper> 
-    {
+    addQuestionWrapper(questionWrapper: QuestionWrapper): Promise<QuestionWrapper> {
         return new Promise((resolve, reject) => {
             try {
-                this.addQuestion(questionWrapper.Text,questionWrapper.CategoryName,questionWrapper.ImageUrl).then((question) => {
-                    console.log(question);
-                    questionWrapper.QuestionId = question.QuestionId;
-                    let promises = new Array<Promise<any>>();
-                    questionWrapper.choice.forEach((element) => {
-                        let promise = this.addChoice(questionWrapper.QuestionId,element.Text).then((choice) => {
-                            element.ChoiceId = choice.ChoiceId;
-                        })
-                        promises.push(promise);
-                    })
-                    Promise.all(promises).then((result) => {
+                if (questionWrapper.Text != "" && questionWrapper.choice.length > 1) {
+                    this.addQuestion(questionWrapper.Text, questionWrapper.CategoryName, questionWrapper.ImageUrl).then((question) => {
+                        console.log(question);
+                        questionWrapper.QuestionId = question.QuestionId;
+                        let promises = new Array<Promise<any>>();
                         questionWrapper.choice.forEach((element) => {
-                           if(element.Text == questionWrapper.correctChoiceText){
-                               console.log(questionWrapper.QuestionId,element.ChoiceId);
-                               this.addCorrectChoice(questionWrapper.QuestionId,element.ChoiceId).then((res) => {
-                                    questionWrapper.correctChoice.ChoiceId = element.ChoiceId;
-                                    questionWrapper.correctChoice.QuestionId = questionWrapper.QuestionId;
-                                    resolve(questionWrapper);
-                               })
-                           }
-                        });
+                            if (element.Text.trim() != "") {
+                                let promise = this.addChoice(questionWrapper.QuestionId, element.Text).then((choice) => {
+                                    element.ChoiceId = choice.ChoiceId;
+                                })
+                                promises.push(promise);
+                            }
+                        })
+                        Promise.all(promises).then((result) => {
+                            questionWrapper.choice.forEach((element) => {
+                                if (element.Text == questionWrapper.correctChoiceText) {
+                                    console.log(questionWrapper.QuestionId, element.ChoiceId);
+                                    this.addCorrectChoice(questionWrapper.QuestionId, element.ChoiceId).then((res) => {
+                                        questionWrapper.correctChoice.ChoiceId = element.ChoiceId;
+                                        questionWrapper.correctChoice.QuestionId = questionWrapper.QuestionId;
+                                        resolve(questionWrapper);
+                                    })
+                                }
+                            });
+                        })
                     })
-                })
+                }
+                else throw new Error("Question Text or Choice found empty.");
             } catch (error) {
                 throw error;
             }
-            
+
         });
     }
 
@@ -155,20 +159,20 @@ export class HTTPService {
      * @param category 
      * it adds the text and category to the database
      */
-    addQuestion(text, category, imageUrl){
-        return this.http.post(`${this.AzureUrl}/api/question`,{ Text: text, CategoryName: category, ImageUrl: imageUrl},this.options).toPromise().then(this.extractData);
+    addQuestion(text, category, imageUrl) {
+        return this.http.post(`${this.AzureUrl}/api/question`, { Text: text, CategoryName: category, ImageUrl: imageUrl }, this.options).toPromise().then(this.extractData);
     }
 
-    addChoice(questionId, text){
-        return this.http.post(`${this.AzureUrl}/api/choice`,{ Text: text, QuestionId: questionId},this.options).toPromise().then(this.extractData);
+    addChoice(questionId, text) {
+        return this.http.post(`${this.AzureUrl}/api/choice`, { Text: text, QuestionId: questionId }, this.options).toPromise().then(this.extractData);
     }
 
-    addUser(user: QuizUser){
-        return this.http.post(`${this.AzureUrl}/api/quizuser`,user,this.options).toPromise().then(this.extractData);
+    addUser(user: QuizUser) {
+        return this.http.post(`${this.AzureUrl}/api/quizuser`, user, this.options).toPromise().then(this.extractData);
     }
 
     addCategory(category: Category): any {
-        return this.http.post(`${this.AzureUrl}/api/category`,category,this.options).toPromise().then(this.extractData);
+        return this.http.post(`${this.AzureUrl}/api/category`, category, this.options).toPromise().then(this.extractData);
     }
 
     /**
@@ -176,8 +180,8 @@ export class HTTPService {
      * @param questionId of which the correct choice is being entered.
      * @param choiceId of which the correct question is being entered.
      */
-    addCorrectChoice(questionId, choiceId){
-        return this.http.post(`${this.AzureUrl}/api/correctChoice`,{ QuestionId: questionId, ChoiceId: choiceId},this.options).toPromise().then(this.extractData);
+    addCorrectChoice(questionId, choiceId) {
+        return this.http.post(`${this.AzureUrl}/api/correctChoice`, { QuestionId: questionId, ChoiceId: choiceId }, this.options).toPromise().then(this.extractData);
     }
 
     /**
@@ -186,16 +190,16 @@ export class HTTPService {
      * It called a HTTP patch method, that contains a JSON object with attributes { questionId: 0, isDelete: true}.
      * @example httpService.deleteQuestion(1).then((data) => { console.log("deleted Succesfully")});
      */
-    deleteQuestion(questionId){
-        return this.http.patch(`${this.AzureUrl}/api/question`,{ QuestionId: questionId, IsDelete: true},this.options).toPromise().then(this.extractData);
+    deleteQuestion(questionId) {
+        return this.http.patch(`${this.AzureUrl}/api/question`, { QuestionId: questionId, IsDelete: true }, this.options).toPromise().then(this.extractData);
     }
 
-    deleteUser(userId){
-        return this.http.delete(`${this.AzureUrl}/api/quizUser?QuizUserId=${userId}`,this.options).toPromise().then(this.extractData);
+    deleteUser(userId) {
+        return this.http.delete(`${this.AzureUrl}/api/quizUser?QuizUserId=${userId}`, this.options).toPromise().then(this.extractData);
     }
 
     deleteCategory(categoryName: any): any {
-        return this.http.delete(`${this.AzureUrl}/api/category?CategoryName=${categoryName}`,this.options).toPromise().then(this.extractData);
+        return this.http.delete(`${this.AzureUrl}/api/category?CategoryName=${categoryName}`, this.options).toPromise().then(this.extractData);
     }
 
     private extractData(res: Response) {
@@ -208,5 +212,5 @@ export class HTTPService {
         console.error('An error occurred', error);
         return Promise.reject(error.message || error);
     }
-    
+
 }
