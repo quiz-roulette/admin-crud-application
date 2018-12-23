c6120ff84e8b438b23b8751012e9591b22247d19
Smit <smitshah95@hotmail.com>
2018-12-23 16:43:21 +0800

    Added loader to ensure to do a check of login before giving access

--- src/app/guard/User.guard.ts
+++ src/app/guard/User.guard.ts
@@ -2,16 +2,26 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router } from '@angular/router';
 import { Server } from 'selenium-webdriver/safari';
 import { HTTPService } from '../service/http.service';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class UserGuard implements CanActivate {
-    constructor(private router: Router, private httpService: HTTPService) { }
+    private loader$ = new Subject<boolean>();
+    public loader = false;
+
+    constructor(private router: Router, private httpService: HTTPService) { 
+        this.loader$.subscribe(loader => {
+            this.loader = loader;
+        });
+    }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
+        this.loader$.next(true);
         try {
             return new Promise((resolve, reject) => {
                 if (localStorage.getItem('user') && localStorage.getItem('password')) {
                     this.httpService.Login(localStorage.getItem('user'), localStorage.getItem('password')).then((res) => {
+                        this.loader$.next(false);
                         if (res == true) {
                             resolve(true);
                         }
